908b505adfb092dc93c95a3e3630052202d057a7
synaxxlove <158541689+synaxxlove@users.noreply.github.com>
2024-08-05 16:55:12 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>⦾ 𝐎𝐰𝐧𝐞𝐫 : <a href='t.me/leackedmmslink'>𝐓𝐮𝐣𝐡𝐞 𝐮𝐬𝐬𝐞 𝐤𝐲 𝐥𝐞𝐧𝐚 🤣</a>\n⦾ 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞: <code>𝐏𝐲𝐭𝐡𝐨𝐧 3 🦄</code>\n⦾ 𝐋𝐢𝐛𝐫𝐚𝐫𝐲 : <a href='https://docs.pyrogram.org/'>𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 {__version__}</a>\n⦾ 𝐔𝐩𝐝𝐚𝐭𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 : <a href='t.me/synaxnetwork'>𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞 🌷</a></b>",
+            text = f"<b>⦾ 𝐎𝐰𝐧𝐞𝐫 : <a href='t.me/HSTv_Official'>𝐓𝐮𝐣𝐡𝐞 𝐮𝐬𝐬𝐞 𝐤𝐲 𝐥𝐞𝐧𝐚 🤣</a>\n⦾ 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞: <code>𝐏𝐲𝐭𝐡𝐨𝐧 3 🦄</code>\n⦾ 𝐋𝐢𝐛𝐫𝐚𝐫𝐲 : <a href='https://docs.pyrogram.org/'>𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 {__version__}</a>\n⦾ 𝐔𝐩𝐝𝐚𝐭𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 : <a href='t.me/HSTv_Official'>𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞 🌷</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
